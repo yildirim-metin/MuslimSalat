8e47b2df1b9b1f95caee5cf033b7f56157bba57b
yildirim-metin <metin_yildirim@outlook.be>
2025-10-15 15:51:27 +0200

    refactoring : rename file of register form

--- MuslimSalat.API/Models/Users/RegisterFormDto.cs
+++ MuslimSalat.API/Models/Users/RegisterFormDto.cs
@@ -2,8 +2,6 @@ namespace MuslimSalat.API.Models.Users;
 
 public record class RegisterFormDto
 {
- 
-
     public required string Username { get; set; }
 
     public required string Password { get; set; }
